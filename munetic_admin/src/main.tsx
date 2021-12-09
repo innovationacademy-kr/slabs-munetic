364a58599c46757d95423fb860b21eb021353fc5
kunlee <leekh716@naver.com>
2021-12-09 20:15:49 +0900

    Revert "Merge branch 'main' of https://github.com/innovationacademy-kr/slabs-munetic into feature/routing-kunlee"
    This reverts commit 8a1ce92c9eb51fa1b4ad5329ee4d45199c5d2b3a, reversing
    changes made to f2f3dd0aa232596690bd526e950ca3032f37aacd.

--- munetic_admin/src/main.tsx
+++ munetic_admin/src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,5 +9,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
--- munetic_app/src/App.tsx
+++ munetic_app/src/App.tsx
@@ -1,4 +1,4 @@
-import Routing from './Routing';
+import Routing from "./Routing";
 
 function App() {
   return (
