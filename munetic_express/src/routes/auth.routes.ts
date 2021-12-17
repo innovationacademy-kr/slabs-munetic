a2e7fb2d8237ef75dc68a4b742672dc3098e03a5
kunlee <leekh716@naver.com>
2021-12-17 12:39:37 +0900

    feat: express make swagger branch

--- munetic_express/src/routes/auth.routes.ts
+++ munetic_express/src/routes/auth.routes.ts
@@ -1,29 +1,8 @@
-/**
- * @swagger
- * tags:
- *   name: Auth
- *   description: 유저 로그인, 회원 가입
- */
 import { Router } from 'express';
 import * as Auth from '../controllers/auth.controller';
 
 export const path = '/auth';
 export const router = Router();
-
-/**
- * @swagger
- *   /api/login:
- *    post:
- *     summary: Login User
- *     produces:
- *      - application/json
- *     parameters:
- *      - id
- *      - password
- *     resoponses:
- *       200:
- *        description: SUCCESS
- */
 
 router.post('/login', Auth.login);
 router.post('/signin', Auth.signin);
