f8cd8b924260ecdc5a5bd837f5d3a880ac92a289
kunlee <leekh716@naver.com>
2021-12-27 22:35:01 +0900

    feat: app #79 loginPage UI&same logic with signup

--- munetic_app/src/lib/api/auth.ts
+++ munetic_app/src/lib/api/auth.ts
@@ -2,4 +2,6 @@ import { userSignupData } from '../../types/userSignupData';
 import client from './client';
 
 export const signup = (userSignupData: userSignupData) =>
-  client.post('/auth/signin', userSignupData);
+  client.post('/auth/signup', userSignupData);
+export const login = (body: { userId: string; password: string }) =>
+  client.post('/auth/login', body);
