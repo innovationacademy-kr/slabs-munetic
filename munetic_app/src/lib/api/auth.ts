c6a98762cf2ea45cd74c9e22a41fa091fc832045
kunlee <leekh716@naver.com>
2021-12-28 18:56:46 +0900

    feat: app #87 register_api_linking

--- munetic_app/src/lib/api/auth.ts
+++ munetic_app/src/lib/api/auth.ts
@@ -2,8 +2,8 @@ import { userSignupData } from '../../types/userSignupData';
 import client from './client';
 
 export const signup = (userSignupData: userSignupData) =>
-  client.post('/auth/signup', userSignupData);
+  client.post('/api/auth/signup', userSignupData);
 export const login = (body: { login_id: string; password: string }) =>
-  client.post('/auth/login', body);
+  client.post('/api/auth/login', body);
 export const isValidInfo = (body: string) =>
-  client.get(`/auth/signup/user?${body}`);
+  client.get(`/api/auth/signup/user?${body}`);
