aec3b5e25eb942b6d1f6458a9f0fd44cb02e1391
kunlee <leekh716@naver.com>
2021-12-28 19:06:09 +0900

    feat: app #87 userId, password changed to login_id

--- munetic_app/src/lib/api/auth.ts
+++ munetic_app/src/lib/api/auth.ts
@@ -3,7 +3,7 @@ import client from './client';
 
 export const signup = (userSignupData: userSignupData) =>
   client.post('/api/auth/signup', userSignupData);
-export const login = (body: { login_id: string; password: string }) =>
+export const login = (body: { login_id: string; login_password: string }) =>
   client.post('/api/auth/login', body);
 export const isValidInfo = (body: string) =>
   client.get(`/api/auth/signup/user?${body}`);
