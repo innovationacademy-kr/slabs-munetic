9a0fdcc34443fefd494e5a898026b452e2a8908b
Jonghyun Lim <ezemango@gmail.com>
2022-01-04 00:31:28 +0900

    fix: proxy, omitted file. envsubst changed 빠트린 파일 추가

--- munetic_proxy/envsubst.sh
+++ munetic_proxy/envsubst.sh
@@ -2,9 +2,9 @@
 set -eu
 
 if [ ${SERVER_HOST}=='localhost' ]; then
-  envsubst '${SERVER_HOST}' < /etc/nginx/templates/localhost.conf.template > /etc/nginx/conf.d/default.conf
+  envsubst '${SERVER_HOST}' < /templates/localhost.conf.template > /etc/nginx/conf.d/default.conf
 else
-  envsubst '${SERVER_HOST}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+  envsubst '${SERVER_HOST}' < /templates/default.conf.template > /etc/nginx/conf.d/default.conf
 fi
 
 exec "$@"
