529246557c1d449af44ad90e9b64a96642de089d
Jonghyun Lim <ezemango@gmail.com>
2022-01-03 22:38:51 +0900

    fix: #105 /admin now shows admin page
    /admin 페이지에 들어가면 어드민 페이지가 제대로 뜨지 않던 버그를
    수정하였습니다.
    빌드된 index.html 내의 script src 경로를 수정하여 해결하였습니다. 자세한
    사항은 이슈에 적어놓겠습니다.

--- munetic_admin/src/main.tsx
+++ munetic_admin/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="admin">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
