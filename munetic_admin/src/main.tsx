08d305d2e3ca0a00e0b4a0c2adb29dafa23c7a9f
chaeinP <pca0046@gmail.com>
2022-01-06 15:45:26 +0900

    Revert "fix:admin #124 Add BrouserRouter basename"
    This reverts commit 412a5e5e3e2422024187a214152752847d62a577.

--- munetic_admin/src/main.tsx
+++ munetic_admin/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/admin">
+    <BrowserRouter>
       <LoginProvider>
         <App />
       </LoginProvider>
