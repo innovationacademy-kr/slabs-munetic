412a5e5e3e2422024187a214152752847d62a577
chaeinP <pca0046@gmail.com>
2022-01-06 15:38:56 +0900

    fix:admin #124 Add BrouserRouter basename

--- munetic_admin/src/main.tsx
+++ munetic_admin/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/admin">
       <LoginProvider>
         <App />
       </LoginProvider>
