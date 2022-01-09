cc5a41be9f13d36b6c1dc427c9ec09060b12a9ec
chaeinP <pca0046@gmail.com>
2022-01-09 23:02:58 +0900

    feat:express #142 create updatePassword api

--- munetic_express/src/controllers/admin/auth.controller.ts
+++ munetic_express/src/controllers/admin/auth.controller.ts
@@ -62,3 +62,14 @@ export const refresh: RequestHandler = async (req, res, next) => {
     next(err);
   }
 };
+
+export const updatePassword: RequestHandler = async (req, res, next) => {
+  try {
+    let { login_password } = req.body;
+    login_password = bcrypt.hashSync(login_password, 10);
+    await UserService.editUserById(req.user!.id, { login_password });
+    res.status(Status.OK).json(new ResJSON('비밀번호가 변경되었습니다.', {}));
+  } catch (err) {
+    next(err);
+  }
+};
