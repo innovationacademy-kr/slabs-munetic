fde5aabb9da53e4b45dca149b88fddd5c981ad03
Kim Hyewon <79592604+hyew-kim@users.noreply.github.com>
2022-05-23 18:16:14 +0900

    feature: #241 add register btn in login page

--- munetic_app/src/pages/auth/LoginPage.tsx
+++ munetic_app/src/pages/auth/LoginPage.tsx
@@ -1,11 +1,35 @@
+import styled from 'styled-components';
 import Login from '../../components/auth/Login';
-import BottomMenu from '../../components/common/BottomMenu';
+import Button from '../../components/common/Button';
+
+const Container = styled.div`
+  width: 60%;
+  margin: 0px auto;
+`;
+
+const RegisterButton = styled(Button)`
+  height: 40px;
+  border-radius: 5px;
+  margin-top: 30px;
+  font-size: 18px;
+  transition: all 0.7s ease;
+  :hover {
+    opacity: 0.8;
+  }
+  ::before {
+    padding-top: 0%;
+  }
+`;
+const CustomP = styled.p`
+  text-align: center;
+`;
 
 export default function LoginPage() {
   return (
-    <>
+    <Container>
       <Login />
-      {/* <BottomMenu /> */}
-    </>
+      <CustomP>OR</CustomP>
+      <RegisterButton children="회원가입" to="/auth/register" />
+    </Container>
   );
 }
